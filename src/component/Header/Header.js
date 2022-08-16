6a0f6bdec7e6af7319326564d5ab2b00ca5935d2
Shaila Nasrin <drshaila2021@gmail.com>
2022-08-16 00:39:24 -0500

    s added

--- src/component/Header/Header.js
+++ src/component/Header/Header.js
@@ -1,12 +1,12 @@
-import React from 'react';
+import React from "react";
 
 //header component
 const Header = () => {
-    return (
-        <div>
-           <h1>Food Village </h1> 
-        </div>
-    );
+  return (
+    <div>
+      <h1>Foods Village </h1>
+    </div>
+  );
 };
 
 export default Header;
